85c5a2811d60c2eda5a5a880bb55f99584acdcf9
PhoenixGitH <miso@MacBook-de-Miso.local>
2017-02-27 11:29:36 +0100

    Todo bien
    Todo correcto

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB || 'mongodb://phoenix:123456@ds153699.mlab.com:53699/shop_test'
+  db: process.env.MONGODB || 'mongodb://phoenix:123456@ds153699.mlab.com:53699/triggers_test'
 }
